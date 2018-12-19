03670d65763035a983c647dc136d6341ea2c62f0
syuta-sugawara <syuta.sugawara@gmail.com>
2018-12-19 16:24:50 +0900

    forms and user accoints

--- blog/tests.py
+++ blog/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+
 
 # Create your tests here.
 
@@ -43,3 +43,29 @@ class BlogTest(TestCase):
         self.assertEqual(no_response.status_code, 404)
         self.assertContains(response,'A good title')
         self.assertTemplateUsed(response,'post_detail.html')
+    
+    def test_get_absolute_url(self):
+        self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+    
+    def test_post_create_view(self):
+        response = self.client.post(reverse('post_new'), {
+            'title':'New title',
+            'body':'New text',
+            'author':self.user,
+        })
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, 'New title')
+        self.assertContains(response, 'New text')
+    
+    def test_post_update_view(self):
+        response=self.client.post(reverse('post_edit',args='1'),{
+            'title':'Update title',
+            'body':'Update text',
+        })
+        self.assertEqual(response.status_code,302)
+    
+    def test_post_delete_view(self):
+        response= self.client.get(
+            reverse('post_delete',args='1'))
+        self.assertEqual(response.status_code,200)
